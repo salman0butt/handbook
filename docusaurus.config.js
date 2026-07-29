fcbfb9dbf68900b218b5cc9f445eba43fac14333
Salman Butt <47062638+salman0butt@users.noreply.github.com>
2026-07-29 23:46:29 +0500

    style(ux): load handbook UX stylesheet

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,10 @@ const config = {
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: [
+            require.resolve('./src/css/custom.css'),
+            require.resolve('./src/css/handbook-ux.css'),
+          ],
         },
       },
     ],
