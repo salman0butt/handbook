31191cbab9f6218d544c6aa88665c8645a7439e1
Salman Butt <47062638+salman0butt@users.noreply.github.com>
2026-07-29 23:43:17 +0500

    feat(ux): add React handbook landing page

---
+++ src/pages/react.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import HandbookLanding from '../components/HandbookLanding';
+import {handbookData} from '../data/handbooks';
+export default function ReactHandbookLanding() { return <HandbookLanding {...handbookData.react} />; }
